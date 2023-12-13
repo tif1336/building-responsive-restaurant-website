2d0f1e9a669666e35228df0dc20fbf282c68b6e1
TheProtectorOfEarth <alyfsakha1@gmail.com>
2023-12-13 17:38:10 +0700

    Menambah web-resto next.js

---
+++ web-resto/src/app/menus/categories/page.js
@@ -0,0 +1,14 @@
+'use client';
+
+import React from 'react';
+import MenuList from '@/components/categories';
+
+const Home = () => {
+  return (
+    <div>
+      <MenuList />
+    </div>
+  );
+};
+
+export default Home;
---
+++ web-resto/src/app/menus/page.js
@@ -0,0 +1,16 @@
+'use client';
+
+import React from 'react';
+import MenuList from '@/components/foodlist';
+
+const Home = () => {
+  return (
+    <div>
+      <MenuList />
+    </div>
+  );
+};
+
+export default Home;
+
+
