0dd3a4402450906381ca08db120732b3e4eb3e0e
TheProtectorOfEarth <alyfsakha1@gmail.com>
2023-12-14 22:20:52 +0700

    Menambah template tailwindcss

--- web-resto/tailwind.config.js
+++ web-resto/tailwind.config.js
@@ -14,5 +14,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/aspect-ratio'),],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
